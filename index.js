6f1b07d22871cc191b0aaad50ec892a3bc86542c
Jarraya-Ahmed <ajarraya@strategya2ai.com>
2024-03-19 15:07:48 +0100

    definin schema and creating resolvers

---
+++ index.js
@@ -0,0 +1,68 @@
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+import { typeDefs } from './schema'
+
+//import database
+import db from './_db'
+
+
+// resolvers
+const resolvers = {
+    Query: {
+      games() {
+        return db.games
+      },
+      game(_, args) {
+        return db.games.find((game) => game.id === args.id)
+      },
+      authors() {
+        return db.authors
+      },
+      author(_, args) {
+        return db.authors.find((author) => author.id === args.id)
+      },
+      reviews() {
+        return db.reviews
+      },
+      review(_, args) {
+        return db.reviews.find((review) => review.id === args.id)
+      }
+    },
+    Game: {
+      reviews(parent) {
+        return db.reviews.filter((r) => r.game_id === parent.id)
+      }
+    },
+    Review: {
+      author(parent) {
+        return db.authors.find((a) => a.id === parent.author_id)
+      },
+      game(parent) {
+        return db.games.find((g) => g.id === parent.game_id)
+      }
+    },
+    Author: {
+      reviews(parent) {
+        return db.reviews.filter((r) => r.author_id === parent.id)
+      }
+    },
+  }
+  
+
+//server setup
+const server= new ApolloServer({typeDefs
+   // typeDefs - definition of types of data game
+  // resolvers
+
+
+
+  
+})
+
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+  })
+  
+  console.log(`Server ready at: ${url}`)
